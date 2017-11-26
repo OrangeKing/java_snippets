db67850de08190150441d031000498ad56504ec2
OrangeKing <theeldu@gmail.com>
2017-11-26 20:21:10 +0100

    Minor revision for lab06;

--- lab06/src/app/dupa.py
+++
@@ -1,1 +0,0 @@
-kupa = "alejaja{}".format(chuj)
--- lab06/src/main.js
+++ lab06/src/main.js
@@ -1,5 +1,8 @@
 #!/usr/bin/jjs
 
+//run from lab core directory 
+//jjs -cp ./src/ src/main.js 
+
 var JavaApp = Java.type('app.App');
 var testValue = JavaApp.testJSMethod('JS');
 print (testValue);
